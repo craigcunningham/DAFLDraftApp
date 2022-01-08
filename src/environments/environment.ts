4f0672080f740d47cb04b339e5743bb41fc5eea0
Craig Cunningham <clcunnin@yahoo.com>
2022-01-07 19:30:19 -0500

    changed api url to azure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://dafldraftapi.azurewebsites.net/public/'
-  apiUrl: 'http://localhost:8000/'
+  apiUrl: 'http://dafldraftapi.azurewebsites.net/public/'
+  //apiUrl: 'http://localhost:8000/'
 };
 
 /*
